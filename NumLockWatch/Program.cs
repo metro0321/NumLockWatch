08da90e756f7e7634b0f5effb9490c461da4761b
metro <melted_snowdrifts@yahoo.co.jp>
2021-07-25 13:59:52 +0900

    ファイル名変更

--- NumLockWatch/Program.cs
+++ NumLockWatch/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using NumLockWatchTest;
+using NumLockWatch;
 using System.Runtime.CompilerServices;
 
 namespace UsbBrowser
@@ -18,7 +18,7 @@ namespace UsbBrowser
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            var form1 = new NumLockWatchTest.Form1();
+            var form1 = new NumLockWatch.Form1();
             Application.Run();
         }
     }
--- NumLockWatch/Properties/Settings.Designer.cs
+++ NumLockWatch/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace NumLockWatchTest.Properties {
+namespace NumLockWatch.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
